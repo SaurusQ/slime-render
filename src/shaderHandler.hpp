96023c451d81f9e5eb418937a1f6192314e5736c
SaurusQ <53377378+SaurusQ@users.noreply.github.com>
2023-05-23 21:24:55 +0300

    last commit before CUDA

--- src/shaderHandler.hpp
+++ src/shaderHandler.hpp
@@ -3,16 +3,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <string>
-#include <vector>
 
 class ShaderHandler
 {
 public:
     ShaderHandler();
     ~ShaderHandler();
-    bool addShader(std::string filename);
+    bool addShader(std::string filename, int shaderType);
     void link();
+    void use();
 private:
     GLuint shaderProgram_;
-    std::vector<GLuint> shaders_;
 };
