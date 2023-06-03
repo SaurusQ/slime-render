af20d97d811cc080ce04d83fa6085b166837bf95
Saurus <313.saurus@gmail.com>
2023-06-03 22:29:48 +0300

    start of make gui

---
+++ src/UI/UI.cpp
@@ -0,0 +1,16 @@
+#include "UI.hpp"
+
+UI::UI()
+{
+    
+}
+
+UI::~UI()
+{
+
+}
+
+void UI::update(GLFWwindow*wnd)
+{
+
+}
---
+++ src/UI/UI.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+class UI
+{
+public:
+    UI();
+    ~UI();
+
+    void update(GLFWwindow* wnd);
+private:
+    
+};
